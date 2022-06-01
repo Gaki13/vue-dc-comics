e7303f3e8e4d6e89e71d21265c18b7d18f00a09d
Gaki13 <gustavoantoniolopez1311@gmail.com>
2022-06-01 19:23:47 +0200

    aggiunta top footer

--- src/App.vue
+++ src/App.vue
@@ -2,18 +2,21 @@
   <div id="app">
     <BaseHeader/>
     <BaseMain/>
+    <TopFooter/>
   </div>
 </template>
 
 <script>
 import BaseHeader from './components/BaseHeader/BaseHeader.vue'
 import BaseMain from './components/BaseMain/ComponentMain.vue'
+import TopFooter from './components/BaseFooter/TopFooter.vue'
 
 export default {
   name: 'App',
   components: {
     BaseHeader,
     BaseMain,
+    TopFooter,
   }
 }
 </script>
---
+++ src/components/BaseFooter/TopFooter.vue
@@ -0,0 +1,46 @@
+<template>
+    <footer >
+        <div class="container-small">
+            <ul class="footer-ul" >
+                <li><img src="../../assets/img/buy-comics-digital-comics.png" alt=""><a href="#">Digitals Comics</a></li>
+                <li><img src="../../assets/img/buy-comics-merchandise.png" alt=""><a href="#">DC Mercahndise</a></li>
+                <li><img src="../../assets/img/buy-comics-subscriptions.png" alt=""><a href="#">Subscription</a></li>
+                <li><img src="../../assets/img/buy-comics-shop-locator.png" alt=""><a href="#">Comic Shop Locator</a></li>
+                <li><img src="../../assets/img/buy-dc-power-visa.svg" alt=""><a href="#">DC Power Visa</a></li>
+            </ul>
+        </div>       
+    </footer>
+</template>
+
+<script>
+export default {
+    name: 'TopFooter' ,
+}
+</script>
+
+<style>
+    footer{
+        height: 25rem;
+        background-color: var(--primary-color);
+        display: flex;
+        align-items: center;
+    }
+
+    .footer-ul{
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        list-style: none;
+        text-transform: uppercase;  
+        gap: 5rem;
+    }
+    .footer-ul li{
+        display: flex;
+        align-items: center;
+        gap: 1rem;
+    }
+    .footer-ul a{
+        text-decoration: none;
+        color: white;
+    }
+</style>

89c7965e30674b00458c7ff949ccefdd505fd312
Gaki13 <gustavoantoniolopez1311@gmail.com>
2022-06-01 17:50:55 +0200

    aggiunta header

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,20 @@
 <template>
   <div id="app">
-  
+    <BaseHeader/>
   </div>
 </template>
 
 <script>
-
+import BaseHeader from './components/BaseHeader/BaseHeader.vue'
 
 export default {
   name: 'App',
   components: {
-    
+    BaseHeader,
   }
 }
 </script>
 
 <style lang="scss">
+@import './assets/style/global.scss'
 </style>
---
+++ src/components/BaseHeader/BaseHeader.vue
@@ -0,0 +1,49 @@
+<template>
+    <header class="container">
+        <!-- Logo dell'header -->
+        <div class="header-left">
+            <img src="../../assets/img/dc-logo.png" alt="Logo di DC Comics">
+        </div>
+        <!-- Navigazione -->
+        <div class="header-right">
+            <nav>   
+                <ul>
+                    <li><a href="#">Characters</a></li>
+                    <li class="active"><a href="#">Comics</a></li>
+                    <li><a href="#">Movies</a></li>
+                    <li><a href="#">TV</a></li>
+                    <li><a href="#">Games</a></li>
+                    <li><a href="#">Collectibiles</a></li>
+                    <li><a href="#">Videos</a></li>
+                    <li><a href="#">Fans</a></li>
+                    <li><a href="#">News</a></li>
+                    <li><a href="#">Shop</a></li>
+                </ul>
+            </nav> 
+        </div>
+    </header>
+</template>
+
+<script>
+export default {
+    name: 'BaseHeader',
+}
+</script>
+
+<style lang="scss" scoped>
+    header{
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        padding: 2.5rem 0;
+        &-right {
+            font-weight: 700;
+        } ul{
+            display: flex;
+            list-style: none;
+            gap: 1.5rem;
+        }a{
+            text-decoration: none;
+        }
+    }
+</style>
